ecef56f734be5092cc67e763e63ecf7adf56c74b
markus <markus@localhost>
2016-11-05 14:23:37 +0100

    -

--- fp_scala/src/Lists.scala
+++ fp_scala/src/Lists.scala
@@ -5,7 +5,7 @@ import scala.annotation.tailrec
 object Lists {
   def main(args : Array[String]) : Unit = {
 
-    println(concat(List(List(1, 2, 3), List(4, 5, 6))))
+    println(hasSubsequence(List[Int](1, 3, 1, 2, 7, 8), List[Int](1, 2)))
   }
 
   def tail[A](list : List[A]) : List[A] =
@@ -57,4 +57,27 @@ object Lists {
 
   def concat[A](lists : List[List[A]]) : List[A] =
     lists.foldLeft(List[A]())((list, acc) => list.foldRight(acc)((elem, acc) => elem :: acc))
+
+  def addOne(list : List[Int]) : List[Int] =
+    list.foldRight(List[Int]())((elem, acc) => elem + 1 :: acc)
+
+  def toString(list : List[Int]) : List[String] =
+    list.foldRight(List[String]())((elem, acc) => elem.toString() :: acc)
+
+  def map[A, B](as : List[A])(f : A => B) : List[B] =
+    as.foldRight(List[B]())((elem, acc) => f(elem) :: acc)
+
+  def filter[A](as : List[A])(f : A => Boolean) : List[A] =
+    as.foldRight(List[A]())((elem, acc) => if (f(elem)) elem :: acc else acc)
+
+  def flatMap[A, B](as : List[A])(f : A => List[B]) : List[B] =
+    as.foldRight(List[B]())((elem, acc) => f(elem) ++ acc)
+
+  def hasSubsequence[A](sup : List[A], sub : List[A]) : Boolean =
+    if (sub.length > sup.length) false
+    else (sup, sub) match {
+      case (supH :: supT, subH :: subT) => if (supH == subH) hasSubsequence(supT, subT) else hasSubsequence(supT, sub)
+      case (Nil, subH :: subT) => false
+      case (_, Nil) => true
+    }
 }
