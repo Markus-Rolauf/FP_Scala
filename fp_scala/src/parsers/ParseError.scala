3310404ea433a8ef863000f7bbcbdc3b0d6d9aa6
markus <markus@localhost>
2016-12-13 21:03:38 +0100

    -

--- fp_scala/src/parsers/ParseError.scala
+++
@@ -1,7 +0,0 @@
-
-
-package parsers
-
-trait ParseError {
-  
-}
--- fp_scala/src/parsers/Parsers.scala
+++ fp_scala/src/parsers/Parsers.scala
@@ -1,11 +1,18 @@
 
 package parsers
 
-trait Parsers[ParseError, Parser[+_]] {
+trait Parsers[ParseError, Parser[+_]] { self =>
 
   def run[A](p : Parser[A])(input : String) : Either[ParseError, A]
-  def char(c : Char) : Parser[Char]
-  def string(s : String) : Parser[String]
   def or[A](s1 : Parser[A], s2 : Parser[A]) : Parser[A]
 
+  implicit def char(c : Char) : Parser[Char]
+  implicit def string(s : String) : Parser[String]
+  implicit def operators[A](p : Parser[A]) = ParserOps[A](p)
+  implicit def asStringParser[A](a : A)(implicit f : A => Parser[String]) : ParserOps[String] = ParserOps(f(a))
+
+  case class ParserOps[A](p : Parser[A]) {
+    def |[B >: A](p2 : Parser[B]) : Parser[B] = self.or(p, p2)
+    def or[B >: A](p2 : Parser[B]) : Parser[B] = self.or(p, p2)
+  }
 }
