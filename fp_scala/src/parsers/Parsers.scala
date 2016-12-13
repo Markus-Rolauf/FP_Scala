1e1cb24a73fac317e300217a828d3a3e6e42aa4e
markus <markus@localhost>
2016-12-13 21:14:58 +0100

    -

--- fp_scala/src/parsers/Parsers.scala
+++ fp_scala/src/parsers/Parsers.scala
@@ -5,6 +5,11 @@ trait Parsers[ParseError, Parser[+_]] { self =>
 
   def run[A](p : Parser[A])(input : String) : Either[ParseError, A]
   def or[A](s1 : Parser[A], s2 : Parser[A]) : Parser[A]
+  def many[A](p : Parser[A]) : Parser[List[A]]
+  def map[A, B](a : Parser[A])(f : A => B) : Parser[B]
+  def slice[A](p : Parser[A]) : Parser[String]
+  def product[A, B](p1 : Parser[A])(p2 : Parser[B]) : Parser[(A, B)]
+  def map2[A, B, C](p1 : Parser[A])(p2 : Parser[B]) : Parser[C]
 
   implicit def char(c : Char) : Parser[Char]
   implicit def string(s : String) : Parser[String]
